a8e38f15ccb4afd19436f31fcffd951abfaf9202
Francis Githae <francis.githae@student.moringaschool.com>
2022-03-04 19:15:44 +0300

    add registration template

--- instagram_mirror/settings.py
+++ instagram_mirror/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'images',
-    'user_profile'
+    'user_profile',
+    'django_bootstrap5',
 ]
+
+ACCOUNT_ACTIVATION_DAYS=1
+REGISTRATION_AUTO_LOGIN=True
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- instagram_mirror/urls.py
+++ instagram_mirror/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('images.urls'))
+    path('',include('images.urls')),
+    re_path('^accounts/',include('registration.backends.default.urls')),
 
 ]
