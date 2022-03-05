fe0dbbb404d8a8d4c3a7366754ece442a8629a6f
Francis Githae <francis.githae@student.moringaschool.com>
2022-03-05 19:15:15 +0300

    register models to admin view,fix static route for media

--- instagram_mirror/urls.py
+++ instagram_mirror/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,re_path,include
 from django.contrib.auth import views
-
+from django.conf.urls.static import static
+from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('images.urls')),
     re_path('^accounts/',include('registration.backends.default.urls')),
 
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
